4d60db9a635244efe109df5c4656d57e3f748d84
Robin <robin.django@gmail.com>
2024-10-26 15:15:03 +0200

    Added placeholder text in visitekaartje window

--- src/lib/components/windows/VisiteKaartje.svelte
+++ src/lib/components/windows/VisiteKaartje.svelte
@@ -1,9 +1,24 @@
 <div>
-  <h3>Robin van der Heijden</h3>
-  <p>Hier komt mijn visitekaartje</p>
+  <hgroup>
+    <h3>Robin van der Heijden</h3>
+    <p>23 - Amsterdam - any pronouns</p>
+  </hgroup>
+  <p>
+    I'm a front-end developer with a passion for design and accessibility. I
+    love creating beautiful and functional websites and applications.
+  </p>
 </div>
 
 <style>
+  div {
+    padding: 1rem;
+    max-width: 20rem;
+  }
+
+  hgroup {
+    margin-bottom: 1rem;
+  }
+
   h3 {
     color: green;
   }
