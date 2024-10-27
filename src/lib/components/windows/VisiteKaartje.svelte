9a4aef41ec5deaab678182a2ddbb68ad99b98265
Robin <robin.django@gmail.com>
2024-10-27 19:44:36 +0100

    New biography text

--- src/lib/components/windows/VisiteKaartje.svelte
+++ src/lib/components/windows/VisiteKaartje.svelte
@@ -4,8 +4,7 @@
     <p>23 - Amsterdam - any pronouns</p>
   </hgroup>
   <p>
-    I'm a front-end developer with a passion for design and accessibility. I
-    love creating beautiful and functional websites and applications.
+    Im Robin! I'm a 23 year old front-end development student at the University of Amsterdam. I have a lot of passion for design and creativity in general. In my free time I occasiionally play video games, and value spending time with my friends. I have a lot of experience with vanilla HTML, CSS and Javascript, and a decent amount of experience with NextJS, React and SvelteKit. Aside from this I'm also very passionate about art and fashion, and enjoy challenging gender norms in clothing and personal expression. I also have a passion for all things retro, and own a record player and collection, a polaroid camera and an old film camera that I love to use.
   </p>
 </div>
 
