14afd2bbb414b54d2c78a27b3d16ec375762c9b4
Robin <robin.django@gmail.com>
2025-02-05 18:01:25 +0100

    Added simple mobile responsiveness

---
+++ src/lib/components/MobileWindowWrapper.svelte
@@ -0,0 +1,83 @@
+<script>
+  export let index;
+  import { openWindows } from "../stores.js";
+</script>
+
+<section>
+  <div class="titlebar">
+    <div class="titlebar-title">
+      <img src={$openWindows[index].icon} alt="Window icon" />
+      <h2 class="unselectable bold">{$openWindows[index].title}</h2>
+    </div>
+    <div class="titlebar-buttons">
+      <div class="minimise"></div>
+      <div class="maximise"></div>
+      <div class="close"></div>
+    </div>
+  </div>
+  <slot></slot>
+</section>
+
+<style>
+  section {
+    margin: 1rem 1rem 0 1rem;
+    background: var(--window-bg);
+    border-image: var(--path-border-default-small);
+    border-style: solid;
+    border-width: 4px;
+    border-image-slice: 4;
+    border-image-repeat: round;
+  }
+
+  .titlebar {
+    display: flex;
+    background-color: var(--window-unfocused);
+    margin: 2px;
+    height: 1.6em;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .titlebar > div {
+    display: flex;
+    align-items: center;
+  }
+
+  .titlebar-title {
+    min-width: 10rem;
+  }
+
+  img {
+    margin-left: 3px;
+    height: 80%;
+  }
+
+  h2 {
+    font-size: 1rem;
+    margin-left: 3px;
+    /* color: white; */
+    --bold-color: #c0c0c0;
+  }
+
+  .titlebar-buttons > div {
+    background-size: cover;
+    width: 1.42rem;
+    height: 1.25rem;
+    /* transform: translateY(-0.5rem); */
+    border: none;
+  }
+
+  .minimise {
+    background-image: var(--path-button-minimise);
+  }
+
+  .maximise {
+    background-image: var(--path-button-maximise);
+    margin-right: 0.1rem;
+  }
+
+  .close {
+    background-image: var(--path-button-close);
+    margin-right: 0.1rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,39 @@
   import Taskbar from "$lib/components/taskbar/Taskbar.svelte";
   import { openWindows } from "$lib/stores.js";
   import Windows from "$lib/components/Windows.svelte";
+  import MobileWindowWrapper from "../lib/components/MobileWindowWrapper.svelte";
 </script>
 
-<div>
+<div class="mobile">
+  <MobileWindowWrapper index={1}>
+    <svelte:component this={$openWindows[3].component} />
+  </MobileWindowWrapper>
+  <MobileWindowWrapper index={0}>
+    <svelte:component this={$openWindows[2].component} />
+  </MobileWindowWrapper>
+  <MobileWindowWrapper index={3}>
+    <svelte:component this={$openWindows[0].component} />
+  </MobileWindowWrapper>
+  <MobileWindowWrapper index={2}>
+    <svelte:component this={$openWindows[1].component} />
+  </MobileWindowWrapper>
+</div>
+
+<div class="desktop">
   <Windows />
   <Taskbar />
 </div>
 
 <style>
-  div {
+  div.desktop {
+    display: none;
+  }
+
+  div.mobile {
+    display: block;
     cursor: url("/cursors/arrow.png"), auto;
-    overflow: hidden;
+    overflow: scroll;
     position: relative;
-    max-width: 1020px;
-    max-height: 660px;
     width: 100%;
     height: 100%;
     background: var(--desktop-bg);
@@ -25,5 +44,29 @@
     border-width: 4px;
     border-image-slice: 4;
     border-image-repeat: round;
+    padding-bottom: 1rem;
+  }
+
+  @media screen and (min-width: 678px) {
+    div.mobile {
+      display: none;
+    }
+
+    div.desktop {
+      display: block;
+      cursor: url("/cursors/arrow.png"), auto;
+      overflow: hidden;
+      position: relative;
+      max-width: 1020px;
+      max-height: 660px;
+      width: 100%;
+      height: 100%;
+      background: var(--desktop-bg);
+      border-image: var(--path-border-default-small);
+      border-style: solid;
+      border-width: 4px;
+      border-image-slice: 4;
+      border-image-repeat: round;
+    }
   }
 </style>
